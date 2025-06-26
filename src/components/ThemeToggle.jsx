dc646181dadf60dcd49d991d6f3016bb750ea681
Hector Rios <103393059+Hrios05@users.noreply.github.com>
2025-06-26 01:31:42 -0700

    finished background star design

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,7 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Sun, Moon } from 'lucide-react'
+import { cn } from '@/lib/utils'
+
+
 export const ThemeToggle = () => {
     // if isDarkMode is true, then setIsDarkMode will operate
     const [isDarkMode, setIsDarkMode] = useState(false)
-    return <button> {isDarkMode ? <Sun /> : <Moon />} </button>
+    //no dependecny means it will run everytime the component renders
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme')
+        if (storedTheme === 'dark') {
+            document.documentElement.classList.add('dark')
+            setIsDarkMode(true)
+
+        }
+        else {
+            document.documentElement.classList.remove('dark')
+            setIsDarkMode(false)
+        }
+
+    }, [])
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.remove('dark')
+            localStorage.setItem('theme', 'light')
+            setIsDarkMode(false)
+        }
+        else {
+            document.documentElement.classList.add('dark')
+            localStorage.setItem('theme', 'dark')
+            setIsDarkMode(true)
+        }
+    }
+    // here we pass in a function toggle theme into on click
+    return <button onClick={toggleTheme} className={cn('fixed max-sm:hidden top-5 right-5 z-5 p-2 rounded-full transition-colors duration-300',
+        'focus:outline-hidden'
+    )}> {isDarkMode ? (<Sun className='h-6 w-6 text-yellow-300' />) : (<Moon className='h-6 w-6 text-blue-900' />)} </button>
 }
